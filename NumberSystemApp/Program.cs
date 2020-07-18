8fc31c9dbc0179aeceb91c3f49e334b573b62024
Daniil-Demyanenko <daniil-demyanenko1@mail.ru>
2020-07-18 22:36:51 +0300

    Добавлен пример использования NumberSystem.cs

--- NumberSystemApp/Program.cs
+++ NumberSystemApp/Program.cs
@@ -10,6 +10,18 @@ namespace NumberSystemApp
     {
         static void Main(string[] args)
         {
+            string numbers = "0123456789" + //Алфавит для перебора
+                "abcdefghijklmnopqrstuvwxyz" +
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+            var NS = new NumberSystem(numbers); //Инициализация
+            NS.setAbcNumber("pf16"); //Установка начального числа
+
+
+            for (int i = 0; i < 20; i++)
+                Console.WriteLine("https://prnt.sc/1n{0}", NS.NextAbc());
+
+            Console.ReadLine();
         }
     }
 }
